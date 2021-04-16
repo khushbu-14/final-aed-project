ee04b31eeea29a68f07b446d58a13444e85327aa
Vivekkumar Singh <singh.vivek@northeastern.edu>
2021-04-15 20:55:17 -0400

    Created crud for FitnessCenterDepartment and FitnessCenterDepartmentDirectory

---
+++ src/Business/FitnessCenter/Department/FitnessCenterDepartment.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.FitnessCenter.Department;
+
+/**
+ *
+ * @author singh
+ */
+public class FitnessCenterDepartment extends Business.UserAccount.UserAccount {
+    
+    private String departmentID;
+    private String departmentName;
+    private String type;
+    private String contact;
+    private String schedule;
+    private String location;
+    private Boolean isRemote;
+
+    public String getDepartmentID() {
+        return departmentID;
+    }
+
+    public void setDepartmentID(String departmentID) {
+        this.departmentID = departmentID;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(String schedule) {
+        this.schedule = schedule;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Boolean getIsRemote() {
+        return isRemote;
+    }
+
+    public void setIsRemote(Boolean isRemote) {
+        this.isRemote = isRemote;
+    }
+    
+    
+    
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return departmentName;
+    }
+    
+}
---
+++ src/Business/FitnessCenter/Department/FitnessCenterDepartmentDirectory.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.FitnessCenter.Department;
+
+import Business.Hospital.Department.HospitalDepartment;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author singh
+ */
+public class FitnessCenterDepartmentDirectory {
+    
+    private ArrayList<FitnessCenterDepartment> departmentList = new ArrayList<>();
+
+    public ArrayList<FitnessCenterDepartment> getDepartmentList() {
+        return departmentList;
+    }
+
+    public void setDepartmentList(ArrayList<FitnessCenterDepartment> departmentList) {
+        this.departmentList = departmentList;
+    }
+    
+    public FitnessCenterDepartment addNewFitnessCenterDepartment(FitnessCenterDepartment fitdept) {
+        departmentList.add(fitdept);
+        return fitdept;
+    }
+    
+    public void deleteFitnessCenterDepartment(FitnessCenterDepartment fitDept){
+        if(departmentList != null){
+            departmentList.remove(fitDept);
+        }
+    }
+    
+         public FitnessCenterDepartment getFitnessCenterDepartmentById(String id){
+         FitnessCenterDepartment fitDept = null;
+         for(FitnessCenterDepartment fitD: departmentList){
+             if(fitD.getDepartmentID().equals(id)){
+                 fitDept = fitD;
+             }    
+         }
+         return fitDept;
+     }
+      public FitnessCenterDepartment getFitnessCenterDepartmentByUserName(String userName){
+         FitnessCenterDepartment fitDept = null;
+         for(FitnessCenterDepartment fitD: departmentList){
+             if(fitD.getUsername().equals(userName)){
+                 fitDept = fitD;
+             }    
+         }
+         return fitDept;
+     }
+      
+       public void updateHospitalByUserName(String userName,String departmentName, String contact,String password, String schedule, String location, Boolean isRemote){
+          FitnessCenterDepartment fit = getFitnessCenterDepartmentByUserName(userName);
+          fit.setDepartmentName(departmentName);
+          //hosp.setType(type);
+          fit.setContact(contact);
+          fit.setPassword(password);
+          fit.setSchedule(schedule);
+          fit.setLocation(location);
+          fit.setIsRemote(isRemote);
+          
+      }
+    
+}
