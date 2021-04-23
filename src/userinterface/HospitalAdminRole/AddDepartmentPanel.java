41b46f251b48c220f69b9751207b81cc59ae07cf
upadhyaykunal <upadhyay.ku@northeastern.edu>
2021-04-22 22:11:55 -0400

    bug fixes

--- src/userinterface/HospitalAdminRole/AddDepartmentPanel.java
+++ src/userinterface/HospitalAdminRole/AddDepartmentPanel.java
@@ -205,6 +205,7 @@ public class AddDepartmentPanel extends javax.swing.JPanel {
 
         errorContactNumber.setForeground(new java.awt.Color(255, 0, 0));
 
+        jComboType.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Select" }));
         jComboType.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jComboTypeActionPerformed(evt);
